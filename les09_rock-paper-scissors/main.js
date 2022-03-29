721c9a355e34e1a568bc7c809d2ef0ff911d5c4b
Korneel Eeckhout <korneel@pienter.tech>
2022-03-29 19:08:40 +0200

    les 8 - rock paper scissors

--- les09_rock-paper-scissors/main.js
+++ les09_rock-paper-scissors/main.js
@@ -1,12 +1,91 @@
-// Generate random 'rock', 'paper' or 'scissors'
-// Ask user for 'rock', 'paper' or 'scissors'
-// Validate user input
-// If tie replay, make sure to generate new random 'rock', 'paper' of 'scissors'
-// Compare values and determine winner
-// Ask if the player wants to play again (not applicable if Extra is active)
-// Alert score
+// VERSION THREE
+// Play a match until x
 
-// Extra:
-// Before game start ask user for 'best of [x]' number
-// Make sure the game only repeats until a winner can be determined
-// Alert who won or if the game was a tie
+function rockPaperScissorsThree(bestOf) {
+    let playAgain = true;
+
+    while (playAgain) {
+        let userScore = 0;
+        let aiScore = 0;
+        
+        while (
+            userScore <= bestOf / 2 &&
+            aiScore <= bestOf / 2 &&
+            userScore + aiScore < bestOf
+        ) {
+            let tie = true;
+            let userChoice;
+            let userChoiceNum;
+            let aiChoice;
+
+            while (tie) {
+                userChoice = prompt('Pick rock, paper or scissors.');
+                aiChoice = Math.floor(Math.random() * 3) + 1;
+
+                while (
+                    userChoice === null ||
+                    (userChoice.toLowerCase() !== 'rock' &&
+                        userChoice.toLowerCase() !== 'paper' &&
+                        userChoice.toLowerCase() !== 'scissors')
+                ) {
+                    userChoice = prompt(
+                        'Invalid input, pick rock, paper or scissors',
+                    );
+                }
+
+                switch (userChoice.toLowerCase()) {
+                    case 'rock':
+                        userChoiceNum = 1;
+                        break;
+                    case 'paper':
+                        userChoiceNum = 2;
+                        break;
+                    case 'scissors':
+                        userChoiceNum = 3;
+                }
+
+                tie = userChoiceNum === aiChoice;
+
+                if (tie) {
+                    alert('Tie, play again.');
+                }
+            }
+
+            if (
+                (userChoiceNum === 1 && aiChoice === 3) ||
+                (userChoiceNum === 2 && aiChoice === 1) ||
+                (userChoiceNum === 3 && aiChoice === 2)
+            ) {
+                userScore++;
+            } else {
+                aiScore++;
+            }
+
+            alert(`AI: ${aiScore} | User: ${userScore}`);
+        }
+
+        if (userScore === aiScore) {
+            playAgain = confirm(`It's a tie, want to play again?`);
+        } else if (userScore > aiScore) {
+            playAgain = confirm('You won, want to play again?');
+        } else {
+            playAgain = confirm('You lost, wnat to play again?');
+        }
+    }
+}
+
+function askForBestOf() {
+    let bestOf = parseInt(prompt('Play until x?'));
+    
+    while (Number.isNaN(bestOf) || bestOf <= 1) {
+        bestOf = parseInt(prompt('Insert a number higher than 0. Play until x?'));
+    } 
+    
+    return bestOf;
+}
+
+let bestOf = askForBestOf();
+
+rockPaperScissorsThree(bestOf);
+
+
---
+++ les10_yahtzee/main.js
@@ -0,0 +1,22 @@
+// Make a yahtzee game
+
+/*
+Lower section
+
+Three Of A Kind	At least three dice the same	                    Sum of all dice
+Four Of A Kind	At least four dice the same	                        Sum of all dice + 2
+Yahtzee	All five dice the same	                                    50
+Full House	Three of one number and two of another	                25
+Small Straight	Four sequential dice (1-2-3-4, 2-3-4-5, or 3-4-5-6)	30
+Large Straight	Five sequential dice (1-2-3-4-5 or 2-3-4-5-6)	    40
+*/
+
+// 1 Generate 5 numbers between 1 and 6
+// 2 Alert numbers
+// 3 Calculate score
+// 4 Alert score + name of throw
+// 5 Ask if player wants to play again?
+// 6 If so start over
+
+// 7 If player does not want to play again
+//      > Alert sum of all previous scores

966099434f80495d13064044a36902f4251d50ba
Korneel Eeckhout <korneel@pienter.tech>
2022-06-21 20:21:59 +0200

    todo solution

--- les20-todo/script.js
+++ les20-todo/script.js
@@ -1,2 +1,147 @@
 // event.target.matches('.done-btn);
 // event.target.closest('.box);
+let $todoList = document.getElementById('todo-list');
+let $todoCount = document.getElementById('todo-count');
+let $doneList = document.getElementById('done-list');
+let $doneCount = document.getElementById('done-count');
+let $textArea = document.getElementById('todo-input');
+let $saveBtn = document.getElementById('save-btn');
+let $clearAllBtn = document.getElementById('clear-all-btn');
+
+/****************
+  DRAW FUNCTIONS
+ ****************/
+
+function drawTodoCount() {
+    $todoCount.innerText = $todoList.children.length;
+}
+
+function drawDoneCount() {
+    $doneCount.innerText = $doneList.children.length;
+}
+
+function drawEmptyTodoList() {
+    $todoList.innerHTML = '';
+    drawTodoCount();
+}
+
+function drawEmptyDoneList() {
+    $doneList.innerHTML = '';
+    drawDoneCount();
+}
+
+function drawNewTodo(text) {
+    let html = `
+    <div class="box">
+        <p>
+            ${text}
+        </p>
+        <button class="done-btn fas fa-check-circle fa-2x"></button>
+    </div>
+    `;
+
+    $todoList.insertAdjacentHTML('beforeend', html);
+    drawTodoCount();
+}
+
+function drawNewDone(text) {
+    let html = `
+    <div class="box">
+        <p>
+            ${text};
+        </p>
+        <button
+            class="remove-btn fas fa-times-circle fa-2x"
+        ></button>
+    </div>
+    `;
+
+    $doneList.insertAdjacentHTML('beforeend', html);
+    drawDoneCount();
+}
+
+function drawEmptyTextArea() {
+    $textArea.value = '';
+}
+
+function removeTodoItem($box) {
+    $box.remove();
+    drawTodoCount();
+}
+
+function removeDoneItem($box) {
+    $box.remove();
+    drawDoneCount();
+}
+
+/***************
+  FUNCTIONALITY
+ ***************/
+
+function init() {
+    drawEmptyTodoList();
+    drawEmptyDoneList();
+    drawEmptyTextArea();
+}
+
+function saveBtnClicked() {
+    let text = $textArea.value;
+    drawNewTodo(text);
+    drawEmptyTextArea();
+}
+
+function clearAllBtnClicked() {
+    drawEmptyTodoList();
+    drawEmptyDoneList();
+}
+
+function doneBtnClicked($curDoneBtn) {
+    let $box = $curDoneBtn.closest('.box');
+    let text = $curDoneBtn.previousElementSibling.innerText;
+
+    removeTodoItem($box);
+    drawNewDone(text);
+}
+
+function boxClicked($box) {
+    if ($box.classList.contains('active')) {
+        return;
+    }
+
+    let $curActiveBox = $todoList.querySelector('.box.active');
+
+    if ($curActiveBox) {
+        $curActiveBox.classList.remove('active');
+    }
+    
+    $box.classList.add('active');
+}
+
+function todoListClicked(event) {
+    if (event.target.matches('.done-btn')) {
+        doneBtnClicked(event.target);
+    }
+
+    if (event.target.matches('.box') || event.target.matches('.box p')) {
+        let $box = event.target.closest('.box');
+        boxClicked($box);
+    }
+}
+
+function doneListClicked(event) {
+    if (event.target.matches('.remove-btn')) {
+        let $box = event.target.closest('.box');
+        removeDoneItem($box);
+    }
+}
+
+/*******
+   RUN
+ *******/
+
+$saveBtn.addEventListener('click', saveBtnClicked);
+$clearAllBtn.addEventListener('click', clearAllBtnClicked);
+$todoList.addEventListener('click', todoListClicked);
+$doneList.addEventListener('click', doneListClicked);
+
+init();
